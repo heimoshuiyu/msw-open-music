a1aa35db4385ff4d8337971dbabf6f29d0775e55
heimoshuiyu <heimoshuiyu@gmail.com>
2021-05-23 14:08:50 +0800

    简介

--- web/index.js
+++ web/index.js
@@ -157,6 +157,9 @@ const component_manage= {
 	},
 	template: `
 <div>
+<h4>关于本站</h4>
+<p>本站是 MSW Project 的一个应用，希望以个人之力分享被隐藏在历史中的音乐。</p>
+<p>自己是V家厨，喜欢的p主包括 wonder-k, buzzG, *luna 等，但却因为种种原因淹没在主流音乐APP的曲库中。本站的初衷是为了让那些知名度低的 VOCALOID / ACG / 东方曲，能够被更多有缘人听到，同时有一个跨平台的工具，能够在低网速的条件下享受硬盘中的无损音乐。</p>
 <component-token :token="token" @set_token="$emit('set_token', $event)"></component-token>
 <table>
 <tbody>
