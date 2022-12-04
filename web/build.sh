ec0dad00efb888b3a202ebd5a78392f94e8d9901
heimoshuiyu <heimoshuiyu@gmail.com>
2022-12-04 19:51:06 +0800

    Replace react with preact
    reduce js file bundle to 20%

--- web/build.sh
+++ web/build.sh
@@ -1,4 +1,6 @@
 rm -rf build
 cp -raf public build
-./node_modules/.bin/esbuild src/index.jsx --bundle --minify --outfile=build/msw-open-music.js --sourcemap
+./node_modules/.bin/esbuild src/index.jsx --bundle --outfile=build/msw-open-music.js --alias:react=preact/compat --alias:react-dom=preact/compat --minify --analyze
 cat public/index.html | sed "s/%PUBLIC_URL%/$PUBLIC_URL/" > build/index.html
+
+echo "Build done, output files under ./build directory"
